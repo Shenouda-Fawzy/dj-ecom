6f657008837de3e5312d1fa446d48560307899ba
Shenouda Fawzy <shenouda.fawzy@hotmail.com>
2024-12-22 16:09:40 +0400

    Template out the website (#5)
    * move navbar to its own file for modularity
    
    * add base.html which is the website skeleton
    
    * clean up the home page to only contains the related part as the common part is moved to the base.html
    
    * add skeleton for about page with the use of store:about
    
    * pupolate about page with dummy lorem ipsum text
    
    * add working links to the navbar

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from store import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.Home, name="home")
+    path('', views.Home, name="home"),
+    path('about/', views.About, name="about"),
 ]
