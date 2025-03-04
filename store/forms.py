09257dca312a957df28126708ad47c13643fa9c5
Shenouda Fawzy <shenouda.fawzy@hotmail.com>
2025-03-04 08:56:43 +0400

    add simple form

---
+++ store/forms.py
@@ -0,0 +1,8 @@
+# Django forms https://docs.djangoproject.com/en/5.1/topics/forms/
+# Django widget https://docs.djangoproject.com/en/5.1/ref/forms/widgets/
+
+from django import forms
+
+class UserCreation(forms.Form):
+    user_name = forms.CharField(label="Your name", max_length=100)
+    user_email = forms.EmailField()
