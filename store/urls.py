bc0eb0d1860b016080dbde0775621694e8abae3b
Shenouda Fawzy <shenouda.fawzy@hotmail.com>
2025-03-16 23:09:59 +0400

    Register User (#7)
    * add url for and view placeholder for register
    
    * add simple form
    
    * fix linting issues
    
    * create register user Django form
    
    * add register form
    
    * fix typo in form name
    
    * style input fields within Django
    
    * add comments
    
    * fix typo in username

--- store/urls.py
+++ store/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('about/', views.About, name="about"),
     path('login/', views.Login_User, name="login"),
     path('logout/', views.Logout_User, name="logout"),
+    path('register/', views.Register_User, name="register")
 ]
