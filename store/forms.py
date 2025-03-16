492a0d96e78868c6250a76a47b7aa06a4bfa16f0
Shenouda Fawzy <shenouda.fawzy@hotmail.com>
2025-03-16 22:29:02 +0400

    style input fields within Django

--- store/forms.py
+++ store/forms.py
@@ -12,9 +12,9 @@ class UserCreation(forms.Form):
 
 
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(label="", widget=forms.TextInput)
-    first_name = forms.CharField(label="", max_length=100)
-    last_name = forms.CharField(label="", max_length=100)
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
 
     class Meta:
 
